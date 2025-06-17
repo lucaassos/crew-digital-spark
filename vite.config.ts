48f7b7698c65cb57102d3ec484c48700f47403e0
lucaassos <152328656+lucaassos@users.noreply.github.com>
2025-06-17 00:59:33 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc' // <-- AQUI ESTÁ A CORREÇÃO
+import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+export default defineConfig({
+  // Caminho base para o GitHub Pages
+  base: '/santa-ilha-imoveis-web/', 
+
+  plugins: [react()],
+
+  // Configuração para o Vite entender os caminhos com "@/"
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+    }
+  }
+})
